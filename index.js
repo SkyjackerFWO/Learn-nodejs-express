f4fe2b5cdb99d2d8813f4d89c570cefc4ad59a6a
Skyjacker22 <infor@coders.skyjacker22>
2019-08-16 17:15:27 +0700

    This is code file default

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+
+const userRoute = require('./routes/user.route');
+
+const port = 3000;
+const app = express();
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.static('public'))
+
+//routes
+app.get('/', (req, res) => {
+    res.render('index',{
+        name : 'AAA'
+    });
+});
+app.use('/users', userRoute);
+
+app.listen(port, () => {
+    console.log('Server running on post ' + port)
+});
