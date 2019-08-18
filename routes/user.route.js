c8326672fd7f139cf42b93a070947e49f695e3f5
Skyjacker22 <infor@coders.skyjacker22>
2019-08-18 22:23:45 +0700

    seSarate the validate section into the validate.js file (middelÆÆware)

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,13 +1,14 @@
 const express = require('express');
 
 const controller = require('../controllers/user.controller');
+const validate = require('../validate/user.validate');
 
-const router = express.Router()
+const router = express.Router();
 router.get('/', controller.index );
 router.get('/search', controller.search);
 router.get('/create',controller.create);
 router.get('/:id', controller.get);
 
-router.post('/create', controller.postCreate);
+router.post('/create', validate.postCreate ,controller.postCreate);
 
 module.exports = router;
