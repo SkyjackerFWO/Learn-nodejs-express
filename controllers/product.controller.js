3694ae72b74926961fa4bc8e62fc71a787eecc2a
Skyjacker22 <infor@coders.skyjacker22>
2019-08-25 11:05:20 +0700

    Pagination 2/2(commplete)

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,5 +1,13 @@
+const db = require('../db');
+
 module.exports.index = (req,res) =>{
+    let page = parseInt(req.query.page) || 1; // n
+    let perPage = 6; //x
+
+    let start = (page - 1)*perPage;
+    let end = page*perPage;
+
     res.render('product/views',{
-        products:db.get('products').value()
+        products:db.get('products').value().slice(start, end)
     });
 };
